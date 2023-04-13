085bd6e039046d009fb1343e4690ae70b1fcccf3
ZhannaZucher <jzucher@gmail.com>
2023-04-13 11:56:31 +0200

    add : dates sort from earliest to latest

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -18,9 +18,14 @@ const row = (bill) => {
     </tr>
     `)
   }
-
+//add sort for dates displaying from earliest to latest
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  return (data && data.length) 
+  ? data
+  .sort((a,b) => new Date(a.date) < new Date(b.date) ? 1 : -1)
+  .map(bill => row(bill))
+  .join("") 
+  : ""
 }
 
 export default ({ data: bills, loading, error }) => {
