51d5e0981b04f09aeebc544561aa931e3d3c4c2b
ZhannaZucher <jzucher@gmail.com>
2023-04-19 11:03:15 +0200

    add: tests configuration and scenarios

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,6 +3,7 @@
  */
 
 import {screen, waitFor} from "@testing-library/dom"
+import "@testing-library/jest-dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
@@ -12,8 +13,9 @@ import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
-
+    
+    //tests configuration
+    beforeEach(() => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -22,18 +24,38 @@ describe("Given I am connected as an employee", () => {
       root.setAttribute("id", "root")
       document.body.append(root)
       router()
+      document.body.innerHTML = BillsUI({ data: bills })
+    })
+
+    test("Then bill icon in vertical layout should be highlighted", async () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
+      expect(windowIcon).toHaveClass("active-icon")
     })
+    
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+
+  describe("When I click on the button 'New Bill'", () => {
+    test("Then I should be redirected to the page 'New Bill'", () => {
+      //test here line 20
+
+    })
+
+    test("Then the function  'HandleCLickIconEye' should be called", () => {
+      //test here line 14
+    })
+    test("Then the modal 'New Bill' should be visible", () => {
+      //test here line 24-27
+
+
     })
   })
 })
